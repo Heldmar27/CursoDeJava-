aa18c057bae155cdc0dc2090bb833a01111096a2
Gastón Montiel <105525139+Heldmar27@users.noreply.github.com>
2022-09-01 06:51:35 -0600

    Update Main.java

--- Lab2Tp10/Main.java
+++ Lab2Tp10/Main.java
@@ -1,5 +1,3 @@
-package com.company;
-
 import javax.swing.*;
 
 public class Main {
@@ -8,6 +6,9 @@ public class Main {
 
         //Inicializo la variable random.
         int numRandom = (int) (Math.random()*100+1);
+        System.out.println(numRandom);
+        //Inicializo el contador para saber la cantidad de intentos.
+        int contador = 0;
 
         //Le pido al usuario que ingrese un número.
         int numUsuario= Integer.parseInt(JOptionPane.showInputDialog("Por favor. Ingrese un número: "));
@@ -15,15 +16,17 @@ public class Main {
 
         //Condiciones para orientar al usuario.
         while (numUsuario != numRandom){
+            contador += 1;
             if (numUsuario<numRandom){
                 numUsuario = Integer.parseInt(JOptionPane.showInputDialog("El número ingresado es menor"));
             } else if (numUsuario>numRandom){
                 numUsuario = Integer.parseInt(JOptionPane.showInputDialog("El número ingresado es mayor"));
             }
+
         }
 
         //Muestro por pantalla que el número es correcto.
-        if (numUsuario == numRandom){JOptionPane.showMessageDialog(null,"El número "+numUsuario+" es correcto!");}
+        if (numUsuario == numRandom){JOptionPane.showMessageDialog(null,"El número "+numUsuario+" es correcto!"+"\nLa cantidad de intentos fueron: "+contador);}
 
 
     }
